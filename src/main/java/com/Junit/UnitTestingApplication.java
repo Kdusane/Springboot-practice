d947ef85d942062611e415767bd5876c31e1a6de
Kdusane <kunaldsn@gmail.com>
2023-01-12 16:38:35 +0530

    new project

---
+++ src/main/java/com/Junit/UnitTestingApplication.java
@@ -0,0 +1,14 @@
+package com.Junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingApplication.class, args);
+		System.out.println("kunal");
+	}
+
+}
